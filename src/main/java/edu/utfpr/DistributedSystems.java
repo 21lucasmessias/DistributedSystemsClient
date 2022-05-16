9ba6611654b8059a55377c510d4da9a56b12e26c
Lucas Messias <21.lucasmessias@gmail.com>
2022-05-15 23:53:03 -0300

    first commit

---
+++ src/main/java/edu/utfpr/DistributedSystems.java
@@ -0,0 +1,13 @@
+package edu.utfpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSystems {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedSystems.class, args);
+    }
+
+}
---
+++ src/main/java/edu/utfpr/infra/SocketClient.java
@@ -0,0 +1,58 @@
+package edu.utfpr.infra;
+
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+@Service
+public class SocketClient {
+
+    public SocketClient() throws IOException, InterruptedException {
+        String serverHostname = "127.0.0.1";
+
+        System.out.println("Attemping to connect to host " + serverHostname + " on port 10008.");
+
+        Socket echoSocket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+
+        try {
+            echoSocket = new Socket(serverHostname, 10008);
+            out = new PrintWriter(echoSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(
+                    echoSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: " + serverHostname);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for "
+                    + "the connection to: " + serverHostname);
+            System.exit(1);
+        }
+
+        BufferedReader stdIn = new BufferedReader(
+                new InputStreamReader(System.in));
+        String userInput;
+
+        System.out.println("Type Message (\"Bye.\" to quit)");
+        while ((userInput = stdIn.readLine()) != null) {
+            out.println(userInput);
+
+            // end loop
+            if (userInput.equals("Bye."))
+                break;
+
+            System.out.println("echo: " + in.readLine());
+        }
+
+        out.close();
+        in.close();
+        stdIn.close();
+        echoSocket.close();
+    }
+}
